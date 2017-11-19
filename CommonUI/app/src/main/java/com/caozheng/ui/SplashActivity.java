40557f1f45b4ad520ea0d0ed357f9d21e19b90d4
CaoZGit <1074401754@qq.com>
2017-11-19 15:35:51 +0800

    修改项目名称

---
+++ CommonUI/app/src/main/java/com/caozheng/ui/SplashActivity.java
@@ -0,0 +1,40 @@
+package com.caozheng.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.caozheng.ui.splashview.SplashView;
+
+/**
+ * @author caozheng
+ * @date 2017/11/19
+ * <p>
+ * describe:
+ */
+public class SplashActivity extends Activity {
+
+    private static final int DURATION_TIME = 3;
+    private static final String IMAGE_URL = "https://ws1.sinaimg.cn/large/d23c7564ly1fg6qckyqxkj20u00zmaf1.jpg";
+    private static final String ACTION_URL = "https://github.com/caozhenggit/CommonUI";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SplashView.showSplashView(this, DURATION_TIME, R.mipmap.ic_launcher, new SplashView.OnSplashViewActionListener() {
+            @Override
+            public void onSplashImageClick(String actionUrl) {
+
+            }
+
+            @Override
+            public void onSplashViewDismiss(boolean initiativeDismiss) {
+                finish();
+            }
+        });
+
+        SplashView.updateSplashData(this, IMAGE_URL, ACTION_URL);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# XAppUI
+# CommonUI
